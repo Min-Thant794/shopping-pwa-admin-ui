77ee1a495dca5ca2635526d580ce68a6dc1ad254
Min-Thant794 <minthanthtun29@gmail.com>
2025-09-12 20:38:22 +0800

    error on login

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    allowedHosts: "df504d03d851.ngrok-free.app"
   }
 })
